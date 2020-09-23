801b8b76d4249617b9d05ad16ea9a749c7c69ccd
ErdosXenia <erdos.xenia@gmail.com>
2020-09-23 14:56:14 +0200

    beolvasas

--- raktar/Program.cs
+++ raktar/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace raktar
 {
     class Program
     {
+        static void BeolvRaktar()
+        {
+            StreamReader raktar = new StreamReader("raktar.csv");
+
+            while (!raktar.EndOfStream)
+            {
+                string[] sor = raktar.ReadLine().Split(';');
+                Console.WriteLine(sor[0]);
+                //sor[0] -> kód
+                //sor[1] -> név
+                //sor[2] -> Ár
+                //sor[3] -> Db
+                //Console.WriteLine(raktar.ReadLine());
+            }
+            raktar.Close();
+        }
+
         static void Main(string[] args)
         {
+            BeolvRaktar();
 
             Console.ReadKey();
         }
