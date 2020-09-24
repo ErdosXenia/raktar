e60ea9c4fbaecb9b52ebeb84be37e2a0006b41d7
ErdosXenia <erdos.xenia@gmail.com>
2020-09-24 08:29:47 +0200

    beolvasas, raktar metodus elkeszitese

--- raktar/Program.cs
+++ raktar/Program.cs
@@ -9,6 +9,8 @@ namespace raktar
 {
     class Program
     {
+        static List<termek> termekek = new List<termek>();
+
         static void BeolvRaktar()
         {
             StreamReader raktar = new StreamReader("raktar.csv");
@@ -16,12 +18,13 @@ namespace raktar
             while (!raktar.EndOfStream)
             {
                 string[] sor = raktar.ReadLine().Split(';');
-                Console.WriteLine(sor[0]);
+                //Console.WriteLine(sor[0]);
                 //sor[0] -> kód
                 //sor[1] -> név
                 //sor[2] -> Ár
                 //sor[3] -> Db
-                //Console.WriteLine(raktar.ReadLine());
+
+                termekek.Add(new termek(sor[0], sor[1], int.Parse(sor[2]), int.Parse(sor[3])));
             }
             raktar.Close();
         }
